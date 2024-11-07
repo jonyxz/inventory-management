3e363917fd5086aef19f12e8da7780a7d129230b
Fachruddin <poenyoex@gmail.com>
2024-11-08 00:20:42 +0700

    add fillable properties and relationships to Item model

--- code/app/Models/Item.php
+++ code/app/Models/Item.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+        'category_id',
+        'supplier_id',
+        'created_by',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
 }
