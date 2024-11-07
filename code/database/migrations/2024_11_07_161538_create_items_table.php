34eab0a314f2b8ffb0e8dfdca9d07722659d6662
Fachruddin <poenyoex@gmail.com>
2024-11-08 00:14:35 +0700

    add items table with name, description, price, quantity, and foreign key references

--- code/database/migrations/2024_11_07_161538_create_items_table.php
+++ code/database/migrations/2024_11_07_161538_create_items_table.php
@@ -13,7 +13,18 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('supplier_id');
+            $table->unsignedBigInteger('created_by');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+            $table->foreign('created_by')->references('id')->on('admins');
         });
     }
 
