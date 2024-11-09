86e6ba4c29eed9544ea5a42c88d8d5d211e312bf
Fachruddin <poenyoex@gmail.com>
2024-11-09 20:55:32 +0700

    add ItemController with CRUD operations for Item model

--- code/app/Http/Controllers/ItemController.php
+++ code/app/Http/Controllers/ItemController.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    /**
+     * Display a listing of the items.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::all();
+        return response()->json($items);
+    }
+
+    /**
+     * Store a newly created item in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = Item::create($request->all());
+        return response()->json($item, 201);
+    }
+
+    /**
+     * Display the specified item.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Item::findOrFail($id);
+        return response()->json($item);
+    }
+
+    /**
+     * Update the specified item in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = Item::findOrFail($id);
+        $item->update($request->all());
+        return response()->json($item);
+    }
+
+    /**
+     * Remove the specified item from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Item::findOrFail($id);
+        $item->delete();
+        return response()->json(null, 204);
+    }
 }
