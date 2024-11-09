7ec1eef13dbd2d18e81085bf866a48309da5217f
Fachruddin <poenyoex@gmail.com>
2024-11-09 20:57:21 +0700

    add SupplierController with CRUD operations for Supplier model

--- code/app/Http/Controllers/SupplierController.php
+++ code/app/Http/Controllers/SupplierController.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Supplier;
 use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    //
+    /**
+     * Display a listing of the suppliers.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $suppliers = Supplier::all();
+        return response()->json($suppliers);
+    }
+
+    /**
+     * Store a newly created supplier in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $supplier = Supplier::create($request->all());
+        return response()->json($supplier, 201);
+    }
+
+    /**
+     * Display the specified supplier.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        return response()->json($supplier);
+    }
+
+    /**
+     * Update the specified supplier in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        $supplier->update($request->all());
+        return response()->json($supplier);
+    }
+
+    /**
+     * Remove the specified supplier from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        $supplier->delete();
+        return response()->json(null, 204);
+    }
 }
