c6ba14ba61d9571f63d9e8a71743c5a4adf7d150
Fachruddin <poenyoex@gmail.com>
2024-11-09 20:59:12 +0700

    add success and error response methods to base Controller

--- code/app/Http/Controllers/Controller.php
+++ code/app/Http/Controllers/Controller.php
@@ -5,8 +5,43 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\JsonResponse;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * Send a success response.
+     *
+     * @param mixed $data
+     * @param string $message
+     * @param int $status
+     * @return JsonResponse
+     */
+    protected function sendSuccess($data, $message = 'Operation successful', $status = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
+
+    /**
+     * Send an error response.
+     *
+     * @param string $message
+     * @param int $status
+     * @param array $errors
+     * @return JsonResponse
+     */
+    protected function sendError($message, $status = 400, $errors = []): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'errors' => $errors,
+        ], $status);
+    }
 }
