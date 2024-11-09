67adc4871bade23b03aec7f9abf02da6e56ac9a7
Fachruddin <poenyoex@gmail.com>
2024-11-09 20:52:44 +0700

    add AdminController with CRUD operations for Admin model

--- code/app/Http/Controllers/AdminController.php
+++ code/app/Http/Controllers/AdminController.php
@@ -2,9 +2,91 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
-    //
+    /**
+     * Display a listing of the admins.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $admins = Admin::all();
+        return response()->json($admins);
+    }
+
+    /**
+     * Store a newly created admin in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:admins',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $admin = Admin::create($request->all());
+        return response()->json($admin, 201);
+    }
+
+    /**
+     * Display the specified admin.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $admin = Admin::findOrFail($id);
+        return response()->json($admin);
+    }
+
+    /**
+     * Update the specified admin in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'sometimes|required|string|max:255',
+            'email' => 'sometimes|required|string|email|max:255|unique:admins,email,' . $id,
+            'password' => 'sometimes|required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $admin = Admin::findOrFail($id);
+        $admin->update($request->all());
+        return response()->json($admin);
+    }
+
+    /**
+     * Remove the specified admin from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $admin = Admin::findOrFail($id);
+        $admin->delete();
+        return response()->json(null, 204);
+    }
 }
