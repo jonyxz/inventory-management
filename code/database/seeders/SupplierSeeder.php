30caf63c5daa902c27c04417d023018c64146da0
rezaaditya16 <rezaramadhanaditya764@gmail.com>
2024-11-14 18:59:29 +0700

    add new suppliers to SupplierSeeder

--- code/database/seeders/SupplierSeeder.php
+++ code/database/seeders/SupplierSeeder.php
@@ -29,5 +29,47 @@ class SupplierSeeder extends Seeder
             'contact_info' => '789 Elm Ave, Somewhere Else',
             'created_by' => 1
         ]);
+
+        Supplier::create([
+            'name' => 'Book World',
+            'contact_info' => '101 Maple St, Booktown',
+            'created_by' => 1
+        ]);
+
+        Supplier::create([
+            'name' => 'Sports Gear Inc.',
+            'contact_info' => '202 Pine St, Sportstown',
+            'created_by' => 1
+        ]);
+
+        Supplier::create([
+            'name' => 'Toy Universe',
+            'contact_info' => '303 Cedar St, Toyland',
+            'created_by' => 1
+        ]);
+
+        Supplier::create([
+            'name' => 'Beauty Essentials',
+            'contact_info' => '404 Birch St, Beautytown',
+            'created_by' => 1
+        ]);
+
+        Supplier::create([
+            'name' => 'Auto Parts Depot',
+            'contact_info' => '505 Spruce St, Autoville',
+            'created_by' => 1
+        ]);
+
+        Supplier::create([
+            'name' => 'Garden Supplies Co.',
+            'contact_info' => '606 Willow St, Gardenville',
+            'created_by' => 1
+        ]);
+
+        Supplier::create([
+            'name' => 'Health Products Ltd.',
+            'contact_info' => '707 Fir St, Healthcity',
+            'created_by' => 1
+        ]);
     }
 }
